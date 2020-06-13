c6b64b47e6df89956dbca65ba71ff9d160a9bad8
Bhornumnard <tak_25908@hotmail.com>
2020-06-13 20:55:55 +0700

    new code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Todolist from './todoClass/todo'
-
+import Todo from './todoClass/todo'
+import {Switch, Route} from 'react-router-dom'
+import {Counter} from './hookCounter/Counter'
+import {TodoList} from './todoHook/TodoList'
 function App() {
   return (
-    <div className="App">
+    <Switch>
+      <Route exact path='/'><div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-       <Todolist />
+       <Todo />
       </header>
-    </div>
+    </div> </Route>
+  <Route path="/hook">
+    <Counter />
+  </Route>
+  <Route path="/todo">
+    <TodoList />
+  </Route>
+    </Switch>
+    
   );
 }
 
